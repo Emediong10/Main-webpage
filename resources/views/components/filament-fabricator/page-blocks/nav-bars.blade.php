27a9ed9ee3f8ebba61f4d18668b28f288597c1ee
Emediong Bassey <basseyemediong@gmail.com>
2024-06-12 14:04:40 +0100

    updated target

--- resources/views/components/filament-fabricator/page-blocks/nav-bars.blade.php
+++ resources/views/components/filament-fabricator/page-blocks/nav-bars.blade.php
@@ -6,24 +6,34 @@
     <div id="header-wrap">
         <div class="container">
             <div class="header-row justify-content-lg-between">
-               <!-- Logo
+                <!-- Logo
                 ============================================= -->
                 <div id="logo">
-                    <a href="index.html" class="standard-logo" data-dark-logo="images/logo-dark.png"><img src="images/logo.png" alt="Canvas Logo"></a>
-                    <a href="index.html" class="retina-logo" data-dark-logo="images/logo-dark@2x.png"><img src="images/logo@2x.png" alt="Canvas Logo"></a>
+                    <a href="#" class="standard-logo" data-dark-logo="images/Aliveng.png"><img
+                            src="{{ asset('images/Aliveng.png') }}" alt="Canvas Logo"></a>
+                    <a href="#" class="retina-logo" data-dark-logo="images/Aliveng.png"><img
+                            src="{{ asset('images/Aliveng.png') }}" alt="Canvas Logo"></a>
                 </div><!-- #logo end -->
 
 
                 <!-- #logo end -->
-               <div class="header-misc">
-                  <!-- Top Search
+                <div class="header-misc">
+                    <!-- Top Search
                     ============================================= -->
 
 
                 </div>
 
                 <div id="primary-menu-trigger">
-                    <svg class="svg-trigger" viewBox="0 0 100 100"><path d="m 30,33 h 40 c 3.722839,0 7.5,3.126468 7.5,8.578427 0,5.451959 -2.727029,8.421573 -7.5,8.421573 h -20"></path><path d="m 30,50 h 40"></path><path d="m 70,67 h -40 c 0,0 -7.5,-0.802118 -7.5,-8.365747 0,-7.563629 7.5,-8.634253 7.5,-8.634253 h 20"></path></svg>
+                    <svg class="svg-trigger" viewBox="0 0 100 100">
+                        <path
+                            d="m 30,33 h 40 c 3.722839,0 7.5,3.126468 7.5,8.578427 0,5.451959 -2.727029,8.421573 -7.5,8.421573 h -20">
+                        </path>
+                        <path d="m 30,50 h 40"></path>
+                        <path
+                            d="m 70,67 h -40 c 0,0 -7.5,-0.802118 -7.5,-8.365747 0,-7.563629 7.5,-8.634253 7.5,-8.634253 h 20">
+                        </path>
+                    </svg>
                 </div>
 
                 <!-- Primary Navigation
@@ -31,41 +41,43 @@
                 <nav class="primary-menu">
                     <ul class="menu-container">
                         @foreach ($nav_items->items as $item)
-                        <li class="menu-item">
-                            <a class="menu-link" href="{{ isset($item['data']['url']) ? $item['data']['url'] : '#' }}">
-                                <div>{{ $item['label'] }}</div>
-                            </a>
-                            @if ($item['children'])
-                            <ul class="sub-menu-container">
-                                @foreach ($item['children'] as $children_item)
-                                <li class="menu-item">
-                                    <a class="menu-link"
-                                        href="{{ isset($children_item['data']['url']) ? $children_item['data']['url'] : '#' }}">
-                                        <div>{{ $children_item['label'] }}</div>
-                                    </a>
-                                    @if ($children_item['children'])
+                            <li class="menu-item">
+                                <a class="menu-link"
+                                    href="{{ isset($item['data']['url']) ? $item['data']['url'] : '#' }}">
+                                    <div>{{ $item['label'] }}</div>
+                                </a>
+                                @if ($item['children'])
                                     <ul class="sub-menu-container">
-                                        @foreach ($children_item['children'] as $grand_child_item)
-                                        <li class="menu-item">
-                                            <a class="menu-link"
-                                                href="{{ isset($grand_child_item['data']['url']) ? $grand_child_item['data']['url'] : '#' }}">
-                                                <div>{{ $grand_child_item['label']}}</div>
-                                            </a>
-                                        </li>
+                                        @foreach ($item['children'] as $children_item)
+                                            <li class="menu-item">
+                                                <a class="menu-link" target = "blank"
+                                                    href="{{ isset($children_item['data']['url']) ? $children_item['data']['url'] : '#' }}">
+                                                    <div>{{ $children_item['label'] }}</div>
+                                                </a>
+                                                @if ($children_item['children'])
+                                                    <ul class="sub-menu-container">
+                                                        @foreach ($children_item['children'] as $grand_child_item)
+                                                            <li class="menu-item">
+                                                                <a class="menu-link" target="blank"
+                                                                    href="{{ isset($grand_child_item['data']['url']) ? $grand_child_item['data']['url'] : '#' }}">
+                                                                    <div>{{ $grand_child_item['label'] }}</div>
+                                                                </a>
+                                                            </li>
+                                                        @endforeach
+                                                    </ul>
+                                                @endif
+                                            </li>
                                         @endforeach
                                     </ul>
-                                    @endif
-                                </li>
-                                @endforeach
-                            </ul>
-                            @endif
-                        </li>
+                                @endif
+                            </li>
                         @endforeach
                     </ul>
                 </nav><!-- #primary-menu end -->
 
                 <form class="top-search-form" action="search.html" method="get">
-                    <input type="text" name="q" class="form-control" value="" placeholder="Type &amp; Hit Enter.." autocomplete="off">
+                    <input type="text" name="q" class="form-control" value=""
+                        placeholder="Type &amp; Hit Enter.." autocomplete="off">
                 </form>
 
             </div>
@@ -74,32 +86,36 @@
     <div class="header-wrap-clone"></div>
 </header><!-- #header end -->
 <style>
-@media (max-width: 564.98px){
-.logo_fit{
-margin-left: 0%;
-}
-}
-/* // Small devices (landscape phones, 576px and up) */
-@media (min-width: 565px) and (max-width: 767.98px) {
-.logo_fit{
-margin-left: 0%;
-}
-}
-/* // Medium devices (tablets, 768px and up) */
-@media (min-width: 768px) and (max-width: 991.98px) {
-.logo_fit{
-margin-left: 0%;
-}
-}
-/* // Large devices (desktops, 992px and up) */
-@media (min-width: 992px) and (max-width: 1199.98px) {
-.logo_fit{
-margin-left: -2%;
-}
-}
-@media (min-width: 1200px) {
-.logo_fit{
-margin-left:-4%
-}
-}
+    @media (max-width: 564.98px) {
+        .logo_fit {
+            margin-left: 0%;
+        }
+    }
+
+    /* // Small devices (landscape phones, 576px and up) */
+    @media (min-width: 565px) and (max-width: 767.98px) {
+        .logo_fit {
+            margin-left: 0%;
+        }
+    }
+
+    /* // Medium devices (tablets, 768px and up) */
+    @media (min-width: 768px) and (max-width: 991.98px) {
+        .logo_fit {
+            margin-left: 0%;
+        }
+    }
+
+    /* // Large devices (desktops, 992px and up) */
+    @media (min-width: 992px) and (max-width: 1199.98px) {
+        .logo_fit {
+            margin-left: -2%;
+        }
+    }
+
+    @media (min-width: 1200px) {
+        .logo_fit {
+            margin-left: -4%
+        }
+    }
 </style>
